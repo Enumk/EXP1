572555cb6728d71ac688c77e8205c77c8e0771e2
Enumk <30385571+Enumk@users.noreply.github.com>
2021-12-20 15:05:33 +0500

    list of 5

--- first.py
+++ first.py
@@ -1,7 +1,7 @@
 
 import random
 print("Welcome ...... ")
-print("Random : ",random.random())
+print("Random : "+'-'.join([random.random() for i in range(5)]))
 print(40*"-")
 for i in li:
     print(i)
