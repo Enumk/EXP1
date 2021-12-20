f4ae8f3794d8fd3c956b0f247346244e33fdee3c
Enumk <30385571+Enumk@users.noreply.github.com>
2021-12-20 15:07:00 +0500

    li addd 5

--- first.py
+++ first.py
@@ -1,7 +1,7 @@
 
 import random
 print("Welcome ...... ")
-print("Random : "+'-'.join([random.random() for i in range(5)]))
+li = [random.random() for i in range(5)]
 print(40*"-")
 for i in li:
     print(i)
